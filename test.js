38960e4a995c3a9e5c72c059fa7255383c5d352a
sp9696 <106443619+sp9696@users.noreply.github.com>
2022-09-19 23:32:29 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-// let toggle=document.getElementsByClassName("toggle-button");
-// let nav_links=document.getElementsByClassName("nav-links");
-
-// // toggle.addEventListener("click", ()=>{nav_links.classList.toggle('active')})
-// toggle.addEventListener("click", ()=>{nav_links.style.display='none'})
-// // $(".toggle-button").click(function change(){$(".nav-links").css("display","none");})
-
