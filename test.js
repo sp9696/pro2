9861d4a34fef36fc30b1255dc328798453f618ed
sp9696 <106443619+sp9696@users.noreply.github.com>
2022-09-19 09:49:10 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+// let toggle=document.getElementsByClassName("toggle-button");
+// let nav_links=document.getElementsByClassName("nav-links");
+
+// // toggle.addEventListener("click", ()=>{nav_links.classList.toggle('active')})
+// toggle.addEventListener("click", ()=>{nav_links.style.display='none'})
+// // $(".toggle-button").click(function change(){$(".nav-links").css("display","none");})
+
